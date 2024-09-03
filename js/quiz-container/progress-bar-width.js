f18b45cdb0c8029c8fcaafc15c24c10d76d3527c
Omar Sanad <omarmohamedsanad3@gmail.com>
2024-09-03 18:05:30 +0300

    Set the progress bar width dynamically

---
+++ js/quiz-container/progress-bar-width.js
@@ -0,0 +1,16 @@
+const progressBarObj = {
+  prgBar: document.querySelector("#quiz-container #questions-progress"),
+  questionsSide: document.querySelector("#quiz-container .questions-side"),
+};
+
+function setProgressBarWidth() {
+  let questionsSideWidth = progressBarObj.questionsSide.offsetWidth;
+  let prgBarWidth = questionsSideWidth * 0.8;
+  progressBarObj.prgBar.style.width = prgBarWidth + "px";
+  progressBarObj.prgBar.style.left = (questionsSideWidth - prgBarWidth) / 2 + "px";
+}
+
+setProgressBarWidth();
+window.addEventListener("resize", () => {
+  setProgressBarWidth();
+});
