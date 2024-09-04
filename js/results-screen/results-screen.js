ace84404e56b432055cdeaee1fc503da91da2cb6
Zayd Elnaghy <zaydelnaghy@gmail.com>
2024-09-05 00:23:50 +0300

    add results screen

--- js/results-screen/results-screen.js
+++ js/results-screen/results-screen.js
@@ -1,25 +1,4 @@
 let theResultsObject = null;
-/* 
-  theResultsObject = {
-    resultsArray: [
-      {
-        question:,
-        answers: (arr) aka options,
-        userAnswer: (string),
-        correctAnswer: (string),
-      },
-      {
-        question:,
-        answers: (arr) aka options,
-        userAnswer: (string),
-        correctAnswer: (string),
-      }
-    ]
-    ,
-    score: (number),
-    timeIsOut: (bool),
-  }
-*/
 
 export function passResultsObject(resultsObject) {
   theResultsObject = resultsObject;
@@ -64,13 +43,88 @@ export function previewResults() {
     quizContainerContent.style.transform = "translateY(200%)";
     quizContainer.style.display = "none";
     /* ============================== render the results screen ============================== */
-    // TODO: ((((display the results screen div))))
-    // TODO:
-    // TODO:
-    // TODO:
+    const resultsScreen = document.getElementById("results");
+    resultsScreen.style.transform = "translateY(0)";
+    document.querySelector("body").style.setProperty("overflow", "auto");
+    const showScore = resultsScreen.querySelector(".show-score");
+    const showAnswers = resultsScreen.querySelector(".show-answers");
+    resultsScreen.classList.add("active");
 
+    showScore.innerHTML = `Your score is: ${theResultsObject.score} / 10`;
+
+    function getAnswerClass(result, answer) {
+      if (
+        result.userAnswer === result.correctAnswer &&
+        result.userAnswer === answer
+      ) {
+        return "correct";
+      }
+      if (
+        result.userAnswer !== result.correctAnswer &&
+        result.userAnswer === answer
+      ) {
+        return "wrong";
+      }
+      if (result.correctAnswer === answer) {
+        return "correct";
+      }
+      return "";
+    }
+    theResultsObject.resultsArray.forEach((result, i) => {
+      const resultDiv = document.createElement("div");
+      resultDiv.classList.add("result");
+      resultDiv.innerHTML = `
+        <div class="question">
+          <h3>Question ${i + 1} <span> </span> </h3>
+          <p>${result.question}</p>
+        </div>
+        <div class = "options">
+          ${result.answers
+            .map(
+              (answer) =>
+                `<button class="option ${getAnswerClass(
+                  result,
+                  answer
+                )}"> ${answer}</button>`
+            )
+            .join("")}
+        </div>`;
+      // Ensure the span element exists before setting its content
+
+      const point = resultDiv.querySelector(".question h3 span");
+      if (point) {
+        // it does not work without this (if statement) I don't know why
+        // Update the span content based on the answer correctness
+        if (getAnswerClass(result, result.userAnswer) === "wrong") {
+          point.textContent = "0 / 1 point";
+        } else {
+          point.textContent = "1 / 1 point";
+        }
+      }
+      showAnswers.appendChild(resultDiv);
+    });
     /* ============================== done rendered the results screen ============================== */
   }, 2000);
-
+  /* 
+  theResultsObject = {
+    resultsArray: [
+      {
+        question:,
+        answers: (arr) aka options,
+        userAnswer: (string),
+        correctAnswer: (string),
+      },
+      {
+        question:,
+        answers: (arr) aka options,
+        userAnswer: (string),
+        correctAnswer: (string),
+      }
+    ]
+    ,
+    score: (number),
+    timeIsOut: (bool),
+  }
+*/
   /* ============================== */
 }
