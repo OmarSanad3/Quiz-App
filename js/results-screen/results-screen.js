6c739c7e9131ead69a66c05919e8ae273a6a90fb
Omar Sanad <omarmohamedsanad3@gmail.com>
2024-09-04 17:52:02 +0300

    prepared for the results screen step, hide the quiz-container totally

--- js/results-screen/results-screen.js
+++ js/results-screen/results-screen.js
@@ -7,13 +7,50 @@ export function passResultsObject(resultsObject) {
 
 export function previewResults() {
   /* Access the DOM elements for the results screen */
-  /* TODO: */
-  /* TODO: */
-  /* TODO: */
-  /* ====================== */
-  /* Do some logic with the {theResultsObject} */
-  /* TODO: */
-  /* TODO: */
-  /* TODO: */
-  /* ====================== */
+  const sideBar = document.querySelector("#quiz-container .side-bar");
+  const prgBar = document.querySelector("#quiz-container #questions-progress");
+  const questionBox = document.querySelector("#quiz-container .question-box");
+  const openSideBarBtn = document.querySelector(
+    "#quiz-container #open-side-bar"
+  );
+  const quizContainerContent = document.querySelector(
+    "#quiz-container .content"
+  );
+  const quizContainer = document.querySelector("#quiz-container");
+  /* ========================================================================================== */
+
+  function hideProgressBar() {
+    prgBar.classList.add("hide");
+  }
+
+  function hideSideBar() {
+    sideBar.classList.add("hide");
+  }
+
+  function hideQuestionBox() {
+    questionBox.classList.add("hide");
+  }
+
+  function hideOpenSideBarBtn() {
+    openSideBarBtn.classList.add("hide");
+  }
+
+  hideProgressBar();
+  hideSideBar();
+  hideQuestionBox();
+  hideOpenSideBarBtn();
+
+  setTimeout(() => {
+    quizContainerContent.style.transform = "translateY(200%)";
+    quizContainer.style.display = "none";
+    /* ============================== render the results screen ============================== */
+    // TODO: ((((display the results screen div))))
+    // TODO: 
+    // TODO:
+    // TODO:
+
+    /* ============================== done rendered the results screen ============================== */
+  }, 2000);
+
+  /* ============================== */
 }
